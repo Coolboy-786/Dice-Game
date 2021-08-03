8023775911b4a95a0bc60af9c0bdf9b64e5662bb
Eshan Jain <80900722+Coolboy-786@users.noreply.github.com>
2021-08-04 03:32:15 +0530

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
+//https://github.com/Coolboy-786/Dice-Game/blob/main/dice1.png
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var randomImageSource = "https://github.com/Coolboy-786/Dice-Game/blob/main/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -13,7 +13,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "https://github.com/Coolboy-786/Dice-Game/blob/main/" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
